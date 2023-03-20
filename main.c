dd495d244b58bbc39c28b00efb7e32311b5ad213
Pedro Emanuel <pedroemanuel_07@hotmail.com>
2023-03-20 16:59:16 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///                 Bacharelado Sistemas de Informação                      ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///           Projeto SIG-Task: Agenda de Tarefas e Compromissos            ///\n");
+    printf("///                Developed by @Pedro-E-Rodrigues-- 2023                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");               
+    printf("///            1. Menu Usuário                                              ///\n");
+    printf("///            2. Menu Agenda                                               ///\n");
+    printf("///            3. Sair                                                      ///\n");
+    return 0; 
+}
